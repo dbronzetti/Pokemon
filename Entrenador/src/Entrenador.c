5c945174aafe65e711777fc931b2c7b786470f68
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2016-08-27 19:51:53 -0300

    Creo el proceso Entrenador

---
+++ Entrenador/src/Entrenador.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : Entrenador.c
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Soy el Entrenador"); /* prints Soy el Entrenador */
+	return EXIT_SUCCESS;
+}
