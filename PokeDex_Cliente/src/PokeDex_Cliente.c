d45b0f4f6d24e41daa7d98c08e4a0601e04350d9
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2016-08-27 19:59:59 -0300

    Creo el proceso PokeDex Cliente

---
+++ PokeDex_Cliente/src/PokeDex_Cliente.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : PokeDex_Cliente.c
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Soy el PokeDex Cliente"); /* prints Soy el PokeDex Cliente */
+	return EXIT_SUCCESS;
+}
