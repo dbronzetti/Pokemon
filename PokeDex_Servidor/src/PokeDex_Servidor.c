b033c35304cfa1a8fcd2ee31ef45c4eb127ce378
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2016-08-27 20:02:44 -0300

    Creo el proceso PokeDex Servidor

---
+++ PokeDex_Servidor/src/PokeDex_Servidor.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : PokeDex_Servidor.c
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Soy el PokeDex Servidor"); /* prints Soy el PokeDex Servidor */
+	return EXIT_SUCCESS;
+}
