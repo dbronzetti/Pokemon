a193b06755c174c39eba29a2c6b7f0920c884911
joelmatiassilva <joelsilva.1987@gmail.com>
2016-09-08 00:42:00 -0300

    libreria de GUI

--- 1_executeMapa.sh
+++ 1_executeMapa.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #####SWAP
-cd /home/utnso/git/tp-2016-2c-CompuMundoHiperMegaRed/Mapa/Debug/
+cd /home/utnso/tp-2016-2c-CompuMundoHiperMegaRed/Mapa/Debug/
 ./Mapa   -l ../logMapa
---
+++ 3_executeMapaGUI.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#####SWAP
+#cambiar por la ruta que es del proyecto
+cd /home/utnso/tp-2016-2c-CompuMundoHiperMegaRed/MapaGUI/Debug/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/tp-2016-2c-CompuMundoHiperMegaRed/nivel/Debug
+./MapaGUI
