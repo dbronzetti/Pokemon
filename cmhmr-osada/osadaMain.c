39701c5721ba2804a614ef937d615cf4ce9be0eb
joelmatiassilva <joelsilva.1987@gmail.com>
2016-09-09 12:04:12 -0300

    refactor de osada

--- cmhmr-osada/osadaMain.c
+++ cmhmr-osada/osadaMain.c
@@ -4,27 +4,42 @@
  *  Created on: 5/9/2016
  *      Author: utnso
  */
-#include <stdint.h>
+
 #include "osada.h"
-#include <stdlib.h>
+
 #include <stdio.h>
 /*
-Antes hay que hacer esto
- truncate --size 100M disco.bin
- ./osada-format disco.bin
+ 	 	 	 	 Antes hay que hacer esto:
+ 	 	 	 	 truncate --size 100M disco.bin
+ 	 	 	 	 ./osada-format disco.bin
 */
-  
-void leerArchivo2(){
-	osada_file *object2=malloc(sizeof( osada_file));
-	FILE * file= fopen("disco.bin", "rb");
-	if (file != NULL) {
-		fread(object2, sizeof(osada_file), 1, file);
-		fclose(file);
-	}
-	printf("%i\n",object2->file_size);
-}
 int main(void) {
-   leerArchivo2();
+	char *ruta = "/home/utnso/tp-2016-2c-CompuMundoHiperMegaRed/cmhmr-osada/disco.bin";
+	osada_file *osadaFile = leerArchivoParaOsadaFile(ruta);
+	osada_header *osadaHeader = leerArchivoParaHeader(ruta);
+
+	printf("Tamanio: %i\n", devolverTamanio(osadaFile));
+	printf("Nombre: %d\n", (unsigned char)devolverNombreDelArchivo(osadaFile));
+	printf("Directorio Padre: %i\n", devolverDirectorioPadre(osadaFile));
+	printf("Ultima Mod: %d\n", devolverUltimaMod(osadaFile));
+	printf("Primer Bloque: %i\n", devolverPrimerBloque(osadaFile));
+
+
+	printf("magic_number: %i\n", devolverMagicNumbre(osadaHeader));
+	printf("version: %i\n", devolverVersion(osadaHeader));
+	printf("fs_blocks: %i\n", devolverFSBloques(osadaHeader));
+	printf("bitmap_blocks: %i\n", devolverBitMapBlocks(osadaHeader));
+	printf("allocations_table_offset: %i\n", devolverAllocations_table_offset(osadaHeader));
+	printf("fs_blocks: %i\n", devolverFSBloques(osadaHeader));
+	printf("data_blocks: %i\n", devolverDataBlocks(osadaHeader));
+	printf("padding: %i\n", devolverPadding(osadaHeader));
+
+
+
+
+	ruta = "";
+	free(osadaFile);
+	free(osadaHeader);
 
 	return 0;
 
