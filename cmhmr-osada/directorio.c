d7f07c38f8a19404b42dcbafb5fe04b3724dfa2b
joelmatiassilva <joelsilva.1987@gmail.com>
2016-09-16 16:39:19 -0300

    lectura de array

---
+++ cmhmr-osada/directorio.c
@@ -0,0 +1,21 @@
+/*
+ * directorio.c
+ *
+ *  Created on: 16/9/2016
+ *      Author: utnso
+ */
+
+
+//Desarrollar las funciones para leer el contenido del árbol de directorios del filesystem OSADA.
+//comprobar cual es root
+//
+
+//LEER LA SECUENCIA DE BLOQUES
+/*
+ * A - LEER first_block
+ * B - IR A TABLA ASIGANACION Y LEER EL BLOQUE
+ * C - EL BLOQUE NOS DA EL PROXIMO BLOQUE A LEER(POSICIONES DEL ARRAY PONELE) Y EL FIN ES FFFFFFF
+ * D - SUPONGO QUE ESOS BLOQUE SON LAS POSICIONES DEL BLOQUE DATOS
+ *
+ *
+ * */
