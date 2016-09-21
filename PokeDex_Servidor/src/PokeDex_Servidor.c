e031eda00c4e4c53560d05f04be966050796e01c
dbronzetti <damianbronzetti@gmail.com>
2016-09-21 00:06:06 -0300

    Pokedex server connections - Pending to test with Pokedex client

--- PokeDex_Servidor/src/PokeDex_Servidor.c
+++ PokeDex_Servidor/src/PokeDex_Servidor.c
@@ -4,10 +4,152 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "PokeDex_Servidor.h"
 
 int main(void) {
+	char *logFile = NULL;
+	pthread_t serverThread;
 	puts("Soy el PokeDex Servidor"); /* prints Soy el PokeDex Servidor */
+
+	pthread_create(&serverThread, NULL, (void*) startServerProg, NULL);
+
+	pthread_join(serverThread, NULL);
+
 	return EXIT_SUCCESS;
+
+}
+
+void startServerProg() {
+	int exitCode = EXIT_FAILURE; //DEFAULT Failure
+	int socketServer;
+
+	exitCode = openServerConnection(PORT, &socketServer);
+	log_info(logPokeDexServer, "SocketServer: %d", socketServer);
+
+	//If exitCode == 0 the server connection is opened and listening
+	if (exitCode == 0) {
+		log_info(logPokeDexServer, "the server is opened");
+
+		exitCode = listen(socketServer, SOMAXCONN);//SOMAXCONN = Constant with the maximum connections allowed by the SO
+
+		if (exitCode < 0) {
+			log_error(logPokeDexServer, "Failed to listen server Port.");
+			return;
+		}
+
+		while (1) {
+			newClients((void*) &socketServer);
+		}
+	}
+
+}
+
+void newClients(void *parameter) {
+	int exitCode = EXIT_FAILURE; //DEFAULT Failure
+
+	t_serverData *serverData = malloc(sizeof(t_serverData));
+	memcpy(&serverData->socketServer, parameter,
+			sizeof(serverData->socketServer));
+
+	// disparar un thread para acceptar cada cliente nuevo (debido a que el accept es bloqueante) y para hacer el handshake
+	/**************************************/
+	//Create thread attribute detached
+	//			pthread_attr_t acceptClientThreadAttr;
+	//			pthread_attr_init(&acceptClientThreadAttr);
+	//			pthread_attr_setdetachstate(&acceptClientThreadAttr, PTHREAD_CREATE_DETACHED);
+	//
+	//			//Create thread for checking new connections in server socket
+	//			pthread_t acceptClientThread;
+	//			pthread_create(&acceptClientThread, &acceptClientThreadAttr, (void*) acceptClientConnection1, &serverData);
+	//
+	//			//Destroy thread attribute
+	//			pthread_attr_destroy(&acceptClientThreadAttr);
+	/************************************/
+
+	exitCode = acceptClientConnection(&serverData->socketServer,&serverData->socketClient);
+
+	if (exitCode == EXIT_FAILURE) {
+		log_warning(logPokeDexServer,"There was detected an attempt of wrong connection");
+		close(serverData->socketClient);
+		free(serverData);
+	} else {
+
+		log_info(logPokeDexServer, "The was received a connection in socket: %d.",serverData->socketClient);
+		//Create thread attribute detached
+		pthread_attr_t handShakeThreadAttr;
+		pthread_attr_init(&handShakeThreadAttr);
+		pthread_attr_setdetachstate(&handShakeThreadAttr,PTHREAD_CREATE_DETACHED);
+
+		//Create thread for checking new connections in server socket
+		pthread_t handShakeThread;
+		pthread_create(&handShakeThread, &handShakeThreadAttr,(void*) handShake, serverData);
+
+		//Destroy thread attribute
+		pthread_attr_destroy(&handShakeThreadAttr);
+
+	}		// END handshakes
+
+}
+
+void handShake(void *parameter) {
+	int exitCode = EXIT_FAILURE; //DEFAULT Failure
+
+	t_serverData *serverData = (t_serverData*) parameter;
+
+	//Receive message size
+	int messageSize = 0;
+	char *messageRcv = malloc(sizeof(messageSize));
+	int receivedBytes = receiveMessage(&serverData->socketClient, messageRcv,sizeof(messageSize));
+
+	//Receive message using the size read before
+	memcpy(&messageSize, messageRcv, sizeof(int));
+	messageRcv = realloc(messageRcv, messageSize);
+	receivedBytes = receiveMessage(&serverData->socketClient, messageRcv,messageSize);
+
+	//starting handshake with client connected
+	t_MessageGenericHandshake *message = malloc(sizeof(t_MessageGenericHandshake));
+	deserializeHandShake(message, messageRcv);
+
+	//Now it's checked that the client is not down
+	if (receivedBytes == 0) {
+		log_error(logPokeDexServer,	"The client went down while handshaking! - Please check the client '%d' is down!",serverData->socketClient);
+		close(serverData->socketClient);
+		free(serverData);
+	} else {
+		switch ((int) message->process) {
+
+		case ENTRENADOR: {
+			log_info(logPokeDexServer, "Message from '%s': %s", getProcessString(message->process), message->message);
+			puts("Ha ingresado un nuevo entrenador");
+			exitCode = sendClientAcceptation(&serverData->socketClient);
+
+			if (exitCode == EXIT_SUCCESS) {
+
+				//Create thread attribute detached
+				pthread_attr_t processMessageThreadAttr;
+				pthread_attr_init(&processMessageThreadAttr);
+				pthread_attr_setdetachstate(&processMessageThreadAttr, PTHREAD_CREATE_DETACHED);
+
+				//Create thread for checking new connections in server socket
+				pthread_t processMessageThread;
+				//pthread_create(&processMessageThread, &processMessageThreadAttr, (void*) processMessageReceived, parameter);
+
+				//Destroy thread attribute
+				pthread_attr_destroy(&processMessageThreadAttr);
+			}
+
+			break;
+		}
+		default: {
+			log_error(logPokeDexServer,	"Process not allowed to connect - Invalid process '%s' tried to connect to MAPA", getProcessString(message->process));
+			close(serverData->socketClient);
+			free(serverData);
+			break;
+		}
+		}
+	}
+
+	free(message->message);
+	free(message);
+	free(messageRcv);
 }
