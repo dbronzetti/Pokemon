8239a8b1827f1b46503b253062dadd16a29391e7
ld14 <luisdavid14@gmail.com>
2016-10-21 22:12:36 -0300

    esqueleto de FUSE en pokedex cliente

--- PokeDex_Cliente/src/PokeDex_Cliente.c
+++ PokeDex_Cliente/src/PokeDex_Cliente.c
@@ -6,6 +6,188 @@
 
 #include "PokeDex_Cliente.h"
 
+const char *full(const char *path) /* Anade un punto de montaje */;//Esto da un warning no importa!
+const char *full(const char *path) /* Anade un punto de montaje */
+{
+
+  char *ep, *buff;
+
+  buff = strdup(path+1); if (buff == NULL) exit(1);
+  //strdup: Esta función devuelve una String compuesta de caracteres repetidos.
+  //El carácter que conforma la cadena es el primer carácter del argumento Character y
+  //se duplica tantas veces como indique Number.
+
+  ep = buff + strlen(buff) - 1; if (*ep == '/') *ep = '\0'; /* Puede que esto no suceda pero ante la duda mejor prevenia */
+
+  if (*buff == '\0') strcpy(buff, "."); /* La magia! */
+
+  return buff;
+}
+
+static void *fuse_init(void)
+{
+  printf("[Fuse] init()\n");
+  // truco para permitir el montaje como una superposición
+  fchdir(save_dir);
+  close(save_dir);
+  return NULL;
+}
+
+
+static int fuse_getattr(const char *path, struct stat *stbuf)
+{
+	int res = 0;
+	path = full(path);
+	printf("[Fuse] getattr(%s)\n", path);
+	memset(stbuf, 0, sizeof(struct stat));
+
+	if (strcmp(path, DIRECTORIO_RAIZ) == 0)
+	{
+		stbuf->st_mode = S_IFDIR | 0777;
+		stbuf->st_nlink = 2;
+	}
+	else
+	{
+		//@TODO: Esta funcion llama al socket y pide el bloque
+		osada_file nodo = obtener_bloque_archivo(path);
+		if (nodo != NULL)
+		{
+			if (nodo.state == 1)
+			{
+				stbuf->st_mode = S_IFREG | 0777;
+				stbuf->st_nlink = 1;
+				stbuf->st_size = nodo.file_size;
+			}
+			else if (nodo.state == 2)
+			{
+				stbuf->st_mode = S_IFDIR | 0777;
+				stbuf->st_nlink = 1;
+			}
+		}
+		else
+		{
+			res = -ENOENT;
+		}
+	}
+
+	return res;
+}
+
+static int fuse_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+	(void) offset;
+
+	int i;
+	osada_file* nodos = obtenerTablaDeArchivos(path);
+
+	if(nodos!=NULL){
+		for (i = 0; i < 1024; i++) 		{
+			if ((nodos[i].state != 0)){
+				filler(buffer, nodos[i].fname, NULL, 0);
+			}
+		}
+	}else{
+		return -ENOENT;
+	}
+
+	return 0;
+}
+
+static int fuse_rmdir(const char* path){
+
+	int resultado = directorioBorrar(path);
+
+	if (resultado!=0)	{
+		printf("[Error_Fuse] rmdir(%s)\n", path);
+		return 1;
+	}
+
+	return resultado;
+}
+
+static int fuse_mkdir(const char* path){
+
+	int resultado = crearDirectorio(path);
+
+	if (resultado!=0)	{
+		printf("[Error_Fuse] mkdir(%s)\n", path);
+		return 1;
+	}
+
+	return resultado;
+}
+
+static int fuse_unlink(const char* path)
+{
+	int resultado = borrarArchivo (path);
+
+	if (resultado!=0)	{
+			printf("[Error_Fuse] unlink(%s)\n", path);
+			return 1;
+		}
+
+		return resultado;
+}
+
+static int fuse_open(const char *path, struct fuse_file_info *fi) {
+    return 0;
+}
+
+static int fuse_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+	osada_file nodos = obtener_bloque_archivo(path);
+
+	//@TODO: ver implementacion.
+	return 0;
+}
+
+static int fuse_write(const char* path, const char* buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+	long int bytes_escritos = 0;
+	bytes_escritos = enviarArchivo(path,offset);
+	return bytes_escritos;
+}
+
+static int fuse_rename (const char *oldname, const char *newName){
+	int resultado = renombrarArchivo(oldname,newName);
+	if (resultado!=0)	{
+			printf("[Error_Fuse] rename(%s,%s)\n", oldname,newName);
+			return 1;
+		}
+
+	return resultado;
+
+}
+
+static struct fuse_operations xmp_oper = {
+    .init       = fuse_init,
+    .getattr	= fuse_getattr,
+    .rmdir		= fuse_rmdir,
+    .readdir	= fuse_readdir,
+    .unlink		= fuse_unlink,
+    .open		= fuse_open,
+    .mkdir		= fuse_mkdir,
+    .rename		= fuse_rename,
+    .write		= fuse_write,
+    .read		= fuse_read,
+//    .access		= fuse_access,
+//    .readlink	= fuse_readlink,
+//    .mknod		= fuse_mknod,
+//    .link		= fuse_link,
+//    .chmod		= fuse_chmod,
+//    .chown		= fuse_chown,
+//    .statfs		= fuse_statfs,
+
+	#ifdef HAVE_SETXATTR
+		.setxattr	= fuse_setxattr,
+		.getxattr	= fuse_getxattr,
+		.listxattr	= fuse_listxattr,
+		.removexattr= fuse_removexattr,
+	#endif
+};
+
+
+
 int main(int argc, char **argv) {
 	char *logFile = NULL;
 	char *disco = string_new();
--- PokeDex_Cliente/src/PokeDex_Cliente.h
+++ PokeDex_Cliente/src/PokeDex_Cliente.h
@@ -1,4 +1,3 @@
-/*
  * PokeDex_Cliente.h
  *
  *  Created on: 30/9/2016
@@ -25,3 +24,52 @@ t_log* logPokeCliente;
 int connectTo(enum_processes processToConnect, int *socketClient);
 
 #endif /* POKEDEX_CLIENTE_H_ */
+
+//Librerias y estructuras pra el FUSE
+#include <asm-generic/errno-base.h>
+#include <bits/socket_type.h>
+#include <fuse/fuse.h>
+#include <fuse/fuse_compat.h>
+#include <netinet/in.h>
+#include <osada.h>
+#include <stddef.h>
+#include <sys/stat.h>
+
+#define FUSE_USE_VERSION 26
+#define DIRECTORIO_RAIZ "/"
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+
+/* para pread()/pwrite() */
+#define _XOPEN_SOURCE 500
+#endif
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+#define PATHLEN_MAX 1024
+
+
+//#include "cache.c"
+
+static char initial_working_dir[PATHLEN_MAX+1] ={ '\0' };
+static char cached_mountpoint[PATHLEN_MAX+1] ={ '\0' };
+static int save_dir;
+
+#ifdef NEVER
+
+static char *local=".";
+// Fin de cosillas para el FUSE
