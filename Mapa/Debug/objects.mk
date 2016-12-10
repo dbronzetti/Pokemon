a8337ed4e6c93feec80a668048d3b15a1cc1d5df
dbronzetti <damianbronzetti@gmail.com>
2016-12-10 17:31:34 -0300

    Adding makefiles Mapa and Entrenador

---
+++ Mapa/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lpthread -lpkmn-battle -lcommons -lnivel-gui -lcurses
+
