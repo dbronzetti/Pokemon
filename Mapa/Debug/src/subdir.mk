581e3d73fd773ceea9c7a8dc2e1b419f7fa4fea7
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2016-12-14 01:41:55 -0300

    Arreglo el directorio del makefile

--- Mapa/Debug/src/subdir.mk
+++ Mapa/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -Icurses -I"/home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed-commons" -include"/home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed-commons/metadata.c" -include"/home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed-commons/sockets.c" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -Icurses -I"/home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed/CompuMundoHiperMegaRed-commons" -include"/home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed/CompuMundoHiperMegaRed-commons/metadata.c" -include"/home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed/CompuMundoHiperMegaRed-commons/sockets.c" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
