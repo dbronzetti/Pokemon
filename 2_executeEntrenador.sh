3c91e8cc816025e6efaff2fd3ee7425fae13c9fc
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2016-12-14 23:43:34 -0300

    Arreglo semaforos en deadlock + borro bash que no sirven mas

--- 2_executeEntrenador.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#####SWAP
-cd /home/utnso/tp-2016-2c-CompuMundoHiperMegaRed/Entrenador/Debug/
-./Entrenador -e Red -p /home/utnso/tp-2016-2c-CompuMundoHiperMegaRed/pokedex  -l ../logEntrenador
