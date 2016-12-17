39d7dfdeddca1d0e9ad0f72abc025899d81209c9
joelmatiassilva <joelsilva.1987@gmail.com>
2016-12-17 08:56:40 -0300

    subo las pruebas ordenadas

---
+++ base/4_Blue.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#####SWAP
+rm /home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed/Entrenador/logEntrenadorBlue
+cd /home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed/Entrenador/Debug/
+./Entrenador -e Blue -p /home/utnso/Documentos/Projects/SO_2016/Github/CompuMundoHiperMegaRed/base  -l ../logEntrenadorBlue
